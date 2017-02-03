430d0e624c81dc352b9786f9c3553bdf23f329f5
Dave Moran <dave.moran118@gmail.com>
2017-02-03 15:37:07 -0500

    Create error messages for other fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -20,11 +20,17 @@ class PostsNew extends Component {
         <div className="form-group">
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
 
         <div className="form-group">
           <label>Content</label>
           <textarea type="text" className="form-control" {...content} />
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -38,6 +44,14 @@ function validate(values) {
 
   if(!values.title) {
     errors.title = 'Enter a username';
+  }
+
+  if(!values.categories) {
+    errors.categories = 'Enter categories';
+  }
+
+  if (!values.content) {
+    errors.content = 'Enter some content';
   }
 
   return errors;
