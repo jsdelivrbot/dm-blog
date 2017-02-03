f2b08600aa79a47196a3aba573d58ad56baba223
Dave Moran <dave.moran118@gmail.com>
2017-02-03 14:34:23 -0500

    Setup form for input from user

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>Create Form</div>
+      <form>
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea type="text" className="form-control" />
+        </div>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNew',
+  fields: [
+    'title',
+    'categories',
+    'content'
+  ]
+})(PostsNew);
